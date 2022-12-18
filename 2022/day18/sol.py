554f21c608c22ac7ab4e04ba352f09ee34419f39
Michael Thramann <mthramann@gmail.com>
2022-12-18 09:27:43 -0500

    Update day 18 solution

--- 2022/day18/sol.py
+++ 2022/day18/sol.py
@@ -12,26 +12,19 @@ for x, y, z in cubes:
         if (t := (x + dx, y + dy, z + dz)) not in cubes:
             adjacent[t] = 1 if t not in adjacent else adjacent[t] + 1
 
-visited = {}
+visited = set()
 stack = []
-group = 0
-outside = -1
-for adj in adjacent:
-    if adj in visited:
-        continue
-    group += 1
-    stack.append(adj)
-    visited[adj] = group
-    while stack:
-        x, y, z = stack.pop()
-        if x == -1 and y == -1 and z == -1:
-            outside = group
-        for dx, dy, dz in deltas:
-            if (t := (x + dx, y + dy, z + dz)) not in cubes and \
-               -1 <= t[0] <= 21 and -1 <= t[1] <= 22 and -1 <= t[2] <= 22 and \
-               t not in visited:
-                visited[t] = group
-                stack.append(t)
+s = (-1, -1, -1) # Assume on outside
+stack.append(s)
+visited.add(s)
+while stack:
+    x, y, z = stack.pop()
+    for dx, dy, dz in deltas:
+        if (t := (x + dx, y + dy, z + dz)) not in cubes and \
+           -1 <= t[0] <= 21 and -1 <= t[1] <= 22 and -1 <= t[2] <= 22 and \
+           t not in visited:
+            visited.add(t)
+            stack.append(t)
 
-exterior = sum([v for k, v in adjacent.items() if visited[k] == outside])
+exterior = sum([v for k, v in adjacent.items() if k in visited])
 print(exterior)
